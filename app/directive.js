d4bbfd63279c6aaeaf56de0d5d7a61820c09986b
nikoloza <nika.tomadze@gmail.com>
2016-09-09 11:20:28 +0400

    angular and d3 working together

--- app/directive.js
+++ app/directive.js
@@ -9,76 +9,77 @@ class ChartDirective {
   }
 
   link (scope, element, attrs) {
-    var svg = d3.select(element)
+    var node = element[0].childNodes[0]
     var width = attrs.width
     var height = attrs.height
+    var svg = d3.select(node)
     var color = d3.scaleOrdinal(d3.schemeCategory20)
 
     var simulation = d3.forceSimulation()
-        .force('link', d3.forceLink().id((d) => d.id ))
-        .force('charge', d3.forceManyBody())
-        .force('center', d3.forceCenter(width / 2, height / 2));
+      .force('link', d3.forceLink().id((d) => d.id ))
+      .force('charge', d3.forceManyBody())
+      .force('center', d3.forceCenter(width / 2, height / 2))
 
-    // d3.json('miserables.json', function(error, graph) {
-    //   if (error) throw error
+    d3.json('data/index.json', (error, data) => {
+      if (error) throw error
 
-    //   var link = svg.append('g')
-    //       .attr('class', 'links')
-    //     .selectAll('line')
-    //     .data(graph.links)
-    //     .enter().append('line')
-    //       .attr('stroke-width', (d) => Math.sqrt(d.value))
+      var link = svg.append('g')
+        .attr('class', 'links')
+        .selectAll('line')
+        .data(data.links)
+        .enter().append('line')
+          .attr('stroke-width', (d) => Math.sqrt(d.value))
 
-    //   var node = svg.append('g')
-    //       .attr('class', 'nodes')
-    //     .selectAll('circle')
-    //     .data(graph.nodes)
-    //     .enter().append('circle')
-    //       .attr('r', 5)
-    //       .attr('fill', (d) => color(d.group))
-    //       .call(d3.drag()
-    //           .on('start', dragstarted)
-    //           .on('drag', dragged)
-    //           .on('end', dragended))
+      var node = svg.append('g')
+        .attr('class', 'nodes')
+        .selectAll('circle')
+        .data(data.nodes)
+        .enter().append('circle')
+          .attr('r', 5)
+          .attr('fill', (d) => color(d.group))
+          .call(d3.drag()
+            .on('start', dragstarted)
+            .on('drag', dragged)
+            .on('end', dragended))
 
-    //   node.append('title')
-    //       .text(function(d) { return d.id; });
+      node.append('title')
+        .text((d) => d.id)
 
-    //   simulation
-    //       .nodes(graph.nodes)
-    //       .on('tick', ticked);
+      simulation
+        .nodes(data.nodes)
+        .on('tick', ticked)
 
-    //   simulation.force('link')
-    //       .links(graph.links);
+      simulation.force('link')
+        .links(data.links)
 
-    //   function ticked() {
-    //     link
-    //         .attr('x1', function(d) { return d.source.x; })
-    //         .attr('y1', function(d) { return d.source.y; })
-    //         .attr('x2', function(d) { return d.target.x; })
-    //         .attr('y2', function(d) { return d.target.y; });
+      function ticked () {
+        link
+          .attr('x1', (d) => d.source.x)
+          .attr('y1', (d) => d.source.y)
+          .attr('x2', (d) => d.target.x)
+          .attr('y2', (d) => d.target.y)
 
-    //     node
-    //         .attr('cx', function(d) { return d.x; })
-    //         .attr('cy', function(d) { return d.y; });
-    //   }
-    // });
+        node
+          .attr('cx', (d) => d.x)
+          .attr('cy', (d) => d.y)
+      }
+    })
 
-    function dragstarted(d) {
-      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-      d.fx = d.x;
-      d.fy = d.y;
+    function dragstarted (d) {
+      if (!d3.event.active) simulation.alphaTarget(0.3).restart()
+      d.fx = d.x
+      d.fy = d.y
     }
 
-    function dragged(d) {
-      d.fx = d3.event.x;
-      d.fy = d3.event.y;
+    function dragged (d) {
+      d.fx = d3.event.x
+      d.fy = d3.event.y
     }
 
-    function dragended(d) {
-      if (!d3.event.active) simulation.alphaTarget(0);
-      d.fx = null;
-      d.fy = null;
+    function dragended (d) {
+      if (!d3.event.active) simulation.alphaTarget(0)
+      d.fx = null
+      d.fy = null
     }
   }
 }
